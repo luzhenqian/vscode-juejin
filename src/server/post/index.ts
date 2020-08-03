2b8d9b45b56c163d0749b7d89c58e5f01d1a92aa
luzhenqian <luzhenqian98@gmail.com>
2020-08-03 11:04:24 +0800

    fix: post

--- src/server/post/index.ts
+++ src/server/post/index.ts
@@ -1,5 +1,10 @@
 import * as axios from "axios";
 import { timeFromNow } from "../../utils/time";
+import {
+  getPostListCateURL,
+  getPostCategoryURL,
+  getPostContentURL,
+} from "../urls/post";
 
 let endCursor = "";
 
@@ -17,9 +22,9 @@ export async function getPostList(
   let { defaultCategory } = variables;
   if (type === GET_POST_LIST_TYPE.INIT && defaultCategory) {
     (await getCategories()).forEach(
-      (_category: { name: string; id: string }) => {
-        if (_category.name === defaultCategory) {
-          category = _category.id;
+      (_category: { category_name: string; category_id: string }) => {
+        if (_category.category_name === defaultCategory) {
+          category = _category.category_id;
         }
       }
     );
@@ -27,52 +32,87 @@ export async function getPostList(
     category = variables.category;
   }
 
-  let url = "https://web-api.juejin.im/query";
-  let after =
+  let cursor =
     type === GET_POST_LIST_TYPE.INIT
-      ? ""
+      ? "0"
       : type === GET_POST_LIST_TYPE.NEXT
       ? endCursor
-      : "";
+      : "0";
+  console.log("id:", category);
 
   let result = await axios.default.post(
-    url,
+    getPostListCateURL,
     {
-      operationName: "",
-      query: "",
-      variables: { first: 20, after, order: "POPULAR", ...variables },
-      extensions: { query: { id: "653b587c5c7c8a00ddf67fc66f989d42" } },
-    },
-    { headers: { "X-Agent": "Juejin/Web" } }
+      cursor,
+      id_type: 2,
+      limit: 20,
+      sort_type: 200,
+      cate_id: category,
+    }
+    // {
+    //   operationName: "",
+    //   query: "",
+    //   variables: { first: 20, after, order: "POPULAR", ...variables },
+    //   extensions: { query: { id: "653b587c5c7c8a00ddf67fc66f989d42" } },
+    // },
+    // { headers: { "X-Agent": "Juejin/Web" } }
   );
   try {
-    const postList = result?.data?.data?.articleFeed?.items?.edges;
-    endCursor = result?.data?.data?.articleFeed?.items?.pageInfo?.endCursor;
-    const postListParser = postList.map((post: any) => {
-      let {
-        id,
-        title,
-        originalUrl,
-        createdAt,
-        user,
-        tags,
-        category,
-        likeCount,
-      } = post?.node;
-      let time = timeFromNow(createdAt);
-      return {
-        id,
-        title,
-        originalUrl,
-        user,
-        tags,
-        category,
-        likeCount,
-        time,
-      };
-    });
-    console.log("postlist parser:", postListParser);
-    return { category, postList: postListParser };
+    let { data } = result.data;
+    let postList = (data as [])
+      .filter((post: any) => {
+        if (
+          post !== undefined &&
+          post.article_id !== undefined &&
+          post.article_info !== undefined
+        )
+          return true;
+        return false;
+      })
+      .map(({ article_id, article_info, author_user_info, category, tags }) => {
+        if (!article_info) return undefined;
+        let { title, digg_count, ctime } = article_info;
+        let { user_name } = author_user_info;
+        return {
+          id: article_id,
+          title,
+          originalUrl: article_id,
+          user: { username: user_name },
+          tags,
+          category,
+          likeCount: digg_count,
+          time: timeFromNow(ctime + "000"),
+        };
+      });
+    console.log("postlist parser:", postList);
+    return { category, postList };
+    // const postList = result?.data?.data?.articleFeed?.items?.edges;
+    // endCursor = result?.data?.data?.articleFeed?.items?.pageInfo?.endCursor;
+    // const postListParser = postList.map((post: any) => {
+    //   let {
+    //     id,
+    //     title,
+    //     originalUrl,
+    //     createdAt,
+    //     user,
+    //     tags,
+    //     category,
+    //     likeCount,
+    //   } = post?.node;
+    //   let time = timeFromNow(createdAt);
+    //   return {
+    //     id,
+    //     title,
+    //     originalUrl,
+    //     user,
+    //     tags,
+    //     category,
+    //     likeCount,
+    //     time,
+    //   };
+    // });
+    // console.log("postlist parser:", postListParser);
+    // return { category, postList: postListParser };
   } catch (e) {
     console.log("err:", e);
     return [];
@@ -82,12 +122,12 @@ export async function getPostList(
 // 获取文章内容
 // 因为掘金禁止了 iframe，只能通过接口获取
 export async function getPost(url: string) {
-  let temp = url.split("/");
-  let postId = temp[temp.length - 1];
-  let detailData = await getDetailData(postId);
-  let result = await axios.default.get(url);
+  // let temp = url.split("/");
+  // let postId = temp[temp.length - 1];
+  // let detailData = await getDetailData(postId);
+  let result = await axios.default.get(getPostContentURL + url);
   try {
-    return { html: result?.data, detailData };
+    return { html: result?.data, detailData: {} };
   } catch (err) {
     return "文章加载出错：" + err.toString();
   }
@@ -95,16 +135,14 @@ export async function getPost(url: string) {
 
 //获取分类
 export async function getCategories() {
-  let result = await axios.default.get(
-    "https://gold-tag-ms.juejin.im/v1/categories",
-    {
-      headers: {
-        "X-Juejin-Src": "web",
-      },
-    }
-  );
+  let result = await axios.default.get(getPostCategoryURL);
   try {
-    return result?.data?.d?.categoryList;
+    (result?.data?.data as []).forEach((d: any) => {
+      let { category_name, category_id } = d;
+      d.name = category_name;
+      d.id = category_id;
+    });
+    return result?.data?.data;
   } catch (err) {
     return "分类加载出错：" + err.toString();
   }
---
+++ src/server/urls/post.ts
@@ -0,0 +1,10 @@
+export const getPostListAllURL =
+  "https://apinew.juejin.im/recommend_api/v1/article/recommend_all_feed"; // 获取全部文章列表
+
+export const getPostListCateURL =
+  "https://apinew.juejin.im/recommend_api/v1/article/recommend_cate_feed"; // 获取分类文章列表
+
+export const getPostCategoryURL =
+  "https://apinew.juejin.im/tag_api/v1/query_category_briefs?show_type=0"; // 获取文章分类
+
+export const getPostContentURL = "https://juejin.im/post/"; // 获取文章内容
