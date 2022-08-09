9c21b31c78e2d24f9c4630065fab91bf4f6d7237
LuZhenqian <luzhenqian98@gmail.com>
2022-08-09 14:48:19 +0800

    feat: post dark mode

--- src/views/app/header.tsx
+++ src/views/app/header.tsx
@@ -12,11 +12,11 @@ function Item({ onClick = () => {}, children }) {
 }
 
 export function Header({ context }) {
-  const { darkMode, setDarkMode } = React.useContext(context);
+  const { darkMode, setDarkMode, reload } = React.useContext(context);
   return (
     <header className="min-h-10 gap2 flex items-center justify-end border-b border-gray-200 p-3 text-gray-500 dark:border-gray-800 dark:bg-gray-900">
       <div className="dark:shadow-highlight/4 inline-flex gap-2 rounded-md p-2 shadow-sm ring-1 ring-gray-900/5 dark:bg-gray-800 dark:text-gray-400 dark:ring-0">
-        <Item>刷新</Item>
+        <Item onClick={reload}>刷新</Item>
         <Item>分类</Item>
         <Item>收藏</Item>
         <Item>阅读设置</Item>
--- src/views/app/post.tsx
+++ src/views/app/post.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import { createDispatch } from "../../flux";
 // import { reducer } from "../reducers/post";
 import { Webview } from "vscode";
@@ -28,11 +28,13 @@ const PostContext = React.createContext<{
   setCategories: Function;
   darkMode: boolean;
   setDarkMode: Function;
+  reload: Function;
 }>({
   categories: [],
   setCategories: () => {},
   darkMode: false,
   setDarkMode: () => {},
+  reload: () => {},
 });
 
 function Categories() {
@@ -50,13 +52,79 @@ function Loading() {
   return <div>Loading...</div>;
 }
 
+function Article() {
+  return (
+    <article className="flex justify-between items-center border-b mt-2 pb-2 gap-2">
+      <div className="flex flex-col flex-1">
+        <div className="mb-1 flex items-center gap-2 text-sm">
+          <a>
+            <img
+              className="h-8 w-8"
+              src="https://miro.medium.com/fit/c/48/48/1*03sCDoeUWtfyReSN91LtAw.jpeg"
+            />
+          </a>
+          <span className="">李小龙</span>
+          <span className="font-light text-gray-600 dark:text-gray-500">
+            2021年2月1日
+          </span>
+        </div>
+
+        <div className="mb-1 flex flex-col">
+          <div className="mb-2 text-xl font-bold cursor-pointer">
+            我遇到了一个有钱的程序员，他给了我 3 条改变人生的建议
+          </div>
+          <div className="font-light cursor-pointer line-clamp-2">
+            永远不要放弃你的工作——在你作为程序员的早期，你的脑海中会有一个一致的感觉，“篱笆另一边的草总是更绿”。永远不要放弃你的工作——在你作为程序员的早期，你的脑海中会有一个一致的感觉，“篱笆另一边的草总是更绿”。永远不要放弃你的工作——在你作为程序员的早期，你的脑海中会有一个一致的感觉，“篱笆另一边的草总是更绿”。永远不要放弃你的工作——在你作为程序员的早期，你的脑海中会有一个一致的感觉，“篱笆另一边的草总是更绿”。永远不要放弃你的工作——在你作为程序员的早期，你的脑海中会有一个一致的感觉，“篱笆另一边的草总是更绿”。永远不要放弃你的工作——在你作为程序员的早期，你的脑海中会有一个一致的感觉，“篱笆另一边的草总是更绿”。
+          </div>
+        </div>
+
+        <div className="flex justify-between text-xs">
+          <div className="flex items-center">
+            <div className="flex gap-2 mr-3">
+              <span className="inline-flex items-center rounded-lg bg-gray-200 px-1.5 py-0.5 cursor-pointer dark:bg-gray-600">
+                前端
+              </span>
+              <span className="inline-flex items-center rounded-lg bg-gray-200 px-1.5 py-0.5 cursor-pointer dark:bg-gray-600">
+                JavaScript
+              </span>
+            </div>
+
+            <div className="flex gap-2">
+              <span>10000人阅读</span>
+              <span>200人点赞</span>
+              <span>122人评论</span>
+            </div>
+          </div>
+          <div className="flex gap-2 text-sm">
+            <span className="cursor-pointer">收藏</span>
+            <span className="cursor-pointer">屏蔽</span>
+          </div>
+        </div>
+      </div>
+
+      <img
+        className="h-16 w-24 object-contain"
+        src="https://miro.medium.com/fit/c/224/224/0*5HWA6yLX2ljJjNq5"
+      />
+    </article>
+  );
+}
+
+function Articles() {
+  return (
+    <main className="flex-1 bg-white px-10 py-2 dark:bg-slate-800">
+      <Article />
+    </main>
+  );
+}
+
 function App() {
   const [categories, setCategories] = React.useState([]);
   const [darkMode, setDarkMode] = React.useState(false);
   React.useEffect(() => {
     dispatch({ type: "GET_CATEGORIES", payload: { setCategories } });
   }, []);
-  console.log('darkMode', darkMode);
+
   return (
     <PostContext.Provider
       value={{
@@ -64,18 +132,26 @@ function App() {
         setCategories,
         darkMode,
         setDarkMode,
+        reload,
       }}
     >
-      <div
-        className={`${
-          darkMode && " dark "
-        } flex h-screen flex-col bg-white text-gray-900 dark:bg-gray-900 dark:text-white`}
-      >
-        <Header context={PostContext} />
-        <div className="flex-1 bg-white dark:bg-slate-800"></div>
+      <div className={`${darkMode && " dark "} `}>
+        <div
+          className={`flex h-screen flex-col bg-white text-gray-900 dark:bg-gray-900 dark:text-white`}
+        >
+          <Header context={PostContext} />
+          <Articles />
+        </div>
       </div>
     </PostContext.Provider>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
+
+function reload() {
+  root.unmount();
+  root.render(<App />);
+}
